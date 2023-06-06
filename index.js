af579fe0d1a26f4e71d84438e7e6866a7051b875
sowmya059 <sowmya.murali@dymensions.io>
2023-06-06 06:53:05 +0530

    updated

---
+++ index.js
@@ -0,0 +1,10 @@
+let person={
+    name:"Sowmya",
+    age:22,
+    country:"India"
+}
+
+function logData(){
+    console.log(person.name +" is "+ person.age+" years old and lives in "+person.country )
+}
+logData()
